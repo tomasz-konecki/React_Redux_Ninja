f4d44d0e244f1abacb4480e097f14bcc28a72c0e
Konecki <koneckit@emea.roche.com>
2018-09-10 13:23:54 +0200

    Finish lesson 27

--- router/src/components/Navbar.js
+++ router/src/components/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, withRouter } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = props => {
+  console.log(props);
+
+  // setTimeout(() => {
+  //   props.history.push("/about");
+  // }, 2000);
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
@@ -22,7 +27,7 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default withRouter(Navbar);
 
 /* 
 Link and Navlink do not send requests to a server (unlike the 'a' tags)
