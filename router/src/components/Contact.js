df90064f9edea27bd6412ec0d71993dccd11ec4a
Konecki <koneckit@emea.roche.com>
2018-09-10 12:26:46 +0200

    Finish lesson 25

---
+++ router/src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Duis ipsum quis et ipsum laboris irure magna nisi. Veniam nulla
+        incididunt aute irure ullamco ipsum amet magna occaecat ullamco eiusmod
+        cupidatat velit ut. Esse sunt voluptate proident aute commodo irure
+        laboris. Irure deserunt qui exercitation qui cillum irure enim anim
+        labore irure. Sint quis dolor deserunt et mollit eiusmod dolor laborum
+        minim duis eiusmod minim. Sit velit ea aute eiusmod culpa veniam
+        cupidatat irure ullamco occaecat nisi enim ea.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
